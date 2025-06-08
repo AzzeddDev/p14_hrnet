eb1fb050bb54bdc6fa4d90572cbcbdd969f8e5cd
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-06-08 14:13:03 +0200

    add condition to link in, header depends on current page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,25 @@
+import {routes} from "../../router/routes";
+import {Link, useLocation} from "react-router-dom";
 
 const Header = () => {
-    return <div>Header</div>
+    const location = useLocation()
+
+    const isEmployeesListPage = location.pathname === routes.employeesList
+    const isHomePage = location.pathname === routes.home
+
+    return (
+        <nav className="navbar">
+            <div className="logo">HRnet</div>
+            <div className="links">
+                {isEmployeesListPage && (
+                    <Link to={routes.home}>Add employees</Link>
+                )}
+                {isHomePage && (
+                    <Link to={routes.employeesList}>View Current Employees</Link>
+                )}
+            </div>
+        </nav>
+    )
 }
 
 export default Header
