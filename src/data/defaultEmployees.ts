d5a6ef63c0c215db18b1627f9f9918eb4e900957
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-08-06 08:43:24 +0200

    add default data for example 2 users

---
+++ src/data/defaultEmployees.ts
@@ -0,0 +1,26 @@
+import { Employee } from "../store/employeesSlice"
+
+export const defaultEmployees: Employee[] = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        dateOfBirth: "1990-05-12",
+        startDate: "2020-01-10",
+        street: "123 Main St",
+        city: "New York",
+        state: "Alaska",
+        zipCode: "10001",
+        department: "Engineering",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Smith",
+        dateOfBirth: "1985-11-23",
+        startDate: "2018-07-01",
+        street: "456 Oak Ave",
+        city: "Los Angeles",
+        state: "Alabama",
+        zipCode: "90001",
+        department: "Marketing",
+    },
+]
