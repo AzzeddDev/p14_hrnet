844b28ed3f7eda7507897fbd7acbdf4e9eb8700b
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-08-06 08:41:59 +0200

    update infos on footer & add current year to footer text

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,11 @@
-
 const Footer = () => {
-    return <div>Footer</div>
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <div className="container-fluid text-center">
+            <p className="footerRegistered">HRnet ® {currentYear} - All Rights Reserved</p>
+        </div>
+    )
 }
 
 export default Footer
