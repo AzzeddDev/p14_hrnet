5def65bc182b87540002aee3d09828055002bfe5
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-08-24 12:05:09 +0200

    add a lazy loading for build lighthouse test

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,19 @@
-import {Route, Routes} from "react-router-dom"
-import {routes} from "./routes"
-import HomePage from "../pages/home/indes"
-import EmployeesListPage from "../pages/employees"
+import { Route, Routes } from "react-router-dom"
+import { routes } from "./routes"
+import { lazy, Suspense } from "react"
+
+// lazy load des pages
+const HomePage = lazy(() => import("../pages/home/indes"))
+const EmployeesListPage = lazy(() => import("../pages/employees"))
 
 const Router = () => {
     return (
-        <Routes>
-            <Route path={routes.home} element={<HomePage />}/>
-            <Route path={routes.employeesList} element={<EmployeesListPage />}/>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path={routes.home} element={<HomePage />} />
+                <Route path={routes.employeesList} element={<EmployeesListPage />} />
+            </Routes>
+        </Suspense>
     )
 }
 
